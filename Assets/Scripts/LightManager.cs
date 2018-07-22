3dd379d605ead244071d4b95ca97adeff2b808af
deyvsonl <dls4@cin.ufpe.br>
2018-07-22 19:41:47 -0300

    Adding the lights

---
+++ Assets/Scripts/LightManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour {
+    private bool _isNight;
+
+	[SerializeField] private GameObject particlePrefab;
+	[SerializeField] private List<Transform> positions; 
+	
+	// Use this for initialization
+	void Start () {
+		Invoke("ActivateNight", 80);
+	}
+	
+	private void ActivateNight(){
+		foreach(var position in positions)
+		{
+			Instantiate(particlePrefab, position.position, position.rotation);
+		}
+	}
+	
+}
