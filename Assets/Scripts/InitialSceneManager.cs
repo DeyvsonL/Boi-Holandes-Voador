d00183d907e6f6f8b69444ec4defe6471d9b1127
Lavínia Paganini <laviniapaganini@gmail.com>
2018-07-22 00:52:25 -0300

    Add initial scene

---
+++ Assets/Scripts/InitialSceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitialSceneManager : MonoBehaviour
+{
+	[SerializeField]
+	private List<GameObject> historyObjects;
+	private int index;
+	
+	private void Start () {
+		
+	}
+	
+	private void Update ()
+	{
+		if (!Input.GetButtonDown("Jump")) return;
+		if (index >= historyObjects.Count - 1)
+		{
+			SceneManager.LoadScene("SampleScene");
+			return;
+		}
+
+		historyObjects[index++].SetActive(false);
+		historyObjects[index].SetActive(true);
+	}
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource gameMusic;
+
+    [SerializeField]
+    private AudioSource secondGameMusic;
+
+    private void Awake()
+    {
+        gameMusic.Play();
+        Invoke("StartSecondMusic", 5);
+    }
+
+    void StartSecondMusic()
+    {
+        secondGameMusic.Play();
+    }
+
+	
+}
