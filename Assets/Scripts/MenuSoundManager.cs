f0dd95077d912dce15f2aeb8f495d3a309b26794
Lavínia Paganini <laviniapaganini@gmail.com>
2018-07-22 16:43:27 -0300

    Create menu sound manager prefab

---
+++ Assets/Scripts/MenuSoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSoundManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioSource menuMusicIntro;
+
+    [SerializeField]
+    private AudioSource menuMusicLoop;
+
+    private void Start()
+    {
+
+        menuMusicIntro.Play();
+        menuMusicLoop.PlayDelayed(menuMusicIntro.clip.length);
+    }
+
+}
