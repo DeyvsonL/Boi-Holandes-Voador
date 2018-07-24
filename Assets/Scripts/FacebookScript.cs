25ed02fe48c2402f5de922d55787b3733fa79a7c
Ian Manor <ianmanor@Ians-MacBook-Pro.local>
2018-07-23 23:01:24 -0300

    adding share buttons to victory and gameover menus

--- Assets/Scripts/FacebookScript.cs
+++ Assets/Scripts/FacebookScript.cs
@@ -3,20 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using Facebook.Unity;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityScript.Lang;
 
-public class FacebookScript : MonoBehaviour {
-
+public class FacebookScript : MonoBehaviour
+{
+	public GameObject ShareButton;
+	
 	// Use this for initialization
 	void Start () {
-		FB.Init();
+		if (Application.platform == RuntimePlatform.WebGLPlayer || Application.platform == RuntimePlatform.Android ||
+		    Application.platform == RuntimePlatform.IPhonePlayer)
+		{
+			FB.Init();
+		}
+		else
+		{
+			ShareButton.SetActive(false);
+		}
 	}
 
 	public void Share()
 	{
-		FB.ShareLink( 
-			new Uri("https://boivoadorpage-boivoador.wedeploy.io/index.html"),
-			callback: ShareCallback
-		);
+		if (Application.platform == RuntimePlatform.WebGLPlayer || Application.platform == RuntimePlatform.Android ||
+		    Application.platform == RuntimePlatform.IPhonePlayer)
+		{
+			FB.ShareLink( 
+				new Uri("https://boivoadorpage-boivoador.wedeploy.io/index.html"),
+				callback: ShareCallback
+			);
+		}
 	}
 
 	private void ShareCallback(IShareResult result)
