3b62d50109495b76e9d2acea7f1e5677c3d4cbf3
deyvsonl <dls4@cin.ufpe.br>
2018-07-23 23:05:53 -0300

    Fixed button on gameOver scene

--- Assets/Scripts/DialogueManagerScript.cs
+++ Assets/Scripts/DialogueManagerScript.cs
@@ -87,19 +87,13 @@ public class DialogueManagerScript : MonoBehaviour
 		}
 		else if (dialogElement.name == "Cristina")
 		{
-			if (dialogElement.name == "Nassau")
-			{
 				Nassau.color = new Color(Nassau.color.r, Nassau.color.g, Nassau.color.b, 0f);
-			}
 			Guilherme.color = new Color(Guilherme.color.r, Guilherme.color.g, Guilherme.color.b, 0f);
 			Cristina.color = new Color(Cristina.color.r, Cristina.color.g, Cristina.color.b, 1f);
 		}
 		else if (dialogElement.name == "Guilherme")
 		{
-			if (dialogElement.name == "Nassau")
-			{
 				Nassau.color = new Color(Nassau.color.r, Nassau.color.g, Nassau.color.b, 0f);
-			}
 			Guilherme.color = new Color(Guilherme.color.r, Guilherme.color.g, Guilherme.color.b, 1f);
 			Cristina.color = new Color(Cristina.color.r, Cristina.color.g, Cristina.color.b, 0f);	
 		}
