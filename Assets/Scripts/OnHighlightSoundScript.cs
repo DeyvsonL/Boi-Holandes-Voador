6c7b480d42e849e55cc62c4813dbb59df05c4ca0
Ian Manor <ianmanor@Ians-MacBook-Pro.local>
2018-07-23 23:36:27 -0300

    button sounds

---
+++ Assets/Scripts/OnHighlightSoundScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnHighlightSoundScript : MonoBehaviour,IPointerEnterHandler, ISelectHandler
+{
+
+	[SerializeField]
+	private AudioSource Source;
+
+
+	public void OnPointerEnter(PointerEventData ped)
+	{
+		Source.Play();
+	}
+
+	public void OnSelect(BaseEventData eventData)
+	{
+		Source.Play();
+	}
+}
--- Assets/Scripts/VictoryMenuScript.cs
+++ Assets/Scripts/VictoryMenuScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class VictoryMenuScript : MonoBehaviour {
+	
 
 	public void ReturnToMainMenu()
 	{
